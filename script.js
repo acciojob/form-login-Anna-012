e87c51760dbdee6506eaee2fe7257487f86bcef8
Anna shabbagh <afreenshabbagh@gmail.com>
2024-11-21 20:52:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+  // Get the first name and last name input values
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
 
+  // Create the full name by concatenating the first and last name
+  const fullName = firstName + " " + lastName;
+
+  // Display the full name using alert()
+  alert(fullName);
 }
